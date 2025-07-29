fd57b42bef9e9ac4e16b05f7d2091f44d9adf3d1
G-OMONDI <glenking194@gmail.com>
2025-07-29 08:04:06 +0300

    var: added terraform variables

--- variable.tf
+++ variable.tf
@@ -1,6 +1,53 @@
 # variables.tf
-variable "vm_ip" {
-  description = "Private IP address for the VM"
-  type        = string
-  default     = "192.168.56.10"
+
+variable "mongo_image" {
+  default = "bitnami/mongodb:6.0"
+}
+
+variable "mongo_container_name" {
+  default = "yolo-mongo"
+}
+
+variable "mongo_port" {
+  default = 27017
+}
+
+variable "mongo_root_password" {
+  default = "your_root_password"
+}
+
+variable "mongo_user" {
+  default = "your_user"
+}
+
+variable "mongo_password" {
+  default = "your_password"
+}
+
+variable "mongo_url" {
+  default = "mongodb://your_user:your_password@mongo:27017/yolo_db?authSource=admin"
+}
+
+variable "backend_image" {
+  default = "glen510/yolo-backend:v1.0.0"
+}
+
+variable "backend_container_name" {
+  default = "yolo-backend"
+}
+
+variable "backend_port" {
+  default = 5000
+}
+
+variable "client_image" {
+  default = "glen510/yolo-client:v1.0.0"
+}
+
+variable "client_container_name" {
+  default = "yolo-client"
+}
+
+variable "client_port" {
+  default = 3000
 }
